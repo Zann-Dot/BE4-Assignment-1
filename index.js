8214978eb310da7557c35f607386337c1e39271e
Zann-Dot <anaykarn234@gmail.com>
2026-04-24 20:29:23 +0530

    feat: implement User and Post models and update entry point to populate author data

--- index.js
+++ index.js
@@ -1,163 +1,44 @@
-const express = require("express");
 const { initializeDatabase } = require("./db/db.connect");
-const Book = require("./models/books.models");
+const Post = require("./models/post.models");
+const Users = require("./models/users.model");
 initializeDatabase();
-const app = express();
-const cors = require("cors");
 
-app.use(cors());
-const PORT = 3000;
-
-app.use(express.json());
-
-//Ex1
-app.post("/books", async (req, res) => {
+const userData = {
+  name: "John",
+  email: "john@gamska.com",
+};
+const addUser = async () => {
   try {
-    const book = req.body;
-
-    if (!book) {
-      return res.status(400).json({ error: "Book data is required" });
-    }
-
-    const newBook = await Book.create(book);
-    res.status(201).json({ message: "Book added successfully", book: newBook });
+    const user = new Users(userData);
+    await user.save();
+    console.log("User added", user);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.log(error);
   }
-});
+};
 
-//Ex3
-app.get("/books", async (req, res) => {
-  const books = await Book.find();
-  if (!books) {
-    return res.status(404).json({ error: "No books found" });
-  }
-  res.status(200).send(books);
-});
-
-//Ex4
-app.get("/books/title/:title", async (req, res) => {
+const postData = {
+  title: "Greetings",
+  content: "Have a good day",
+  author: "69eb2055f3df0ac0c4f12701",
+};
+const addPost = async () => {
   try {
-    const book = await Book.findOne({ title: req.params.title });
-
-    if (!book) {
-      return res.status(404).json({ error: "Book not found" });
-    }
-    res.status(200).send(book);
+    const posts = new Post(postData);
+    await posts.save();
+    console.log("Post added");
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.log(error);
   }
-});
+};
+// addPost();
 
-//Ex5
-app.get("/books/author/:author", async (req, res) => {
+const getPosts = async () => {
   try {
-    const book = await Book.find({ author: req.params.author });
-
-    if (!book) {
-      return res.status(404).json({ error: "Book not found" });
-    }
-    res.status(200).send(book);
+    const posts = await Post.find().populate("author");
+    console.log(posts);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.log(error);
   }
-});
-
-//Ex6
-app.get("/books/genre/:genre", async (req, res) => {
-  try {
-    const book = await Book.find({ genre: req.params.genre });
-
-    if (!book) {
-      return res.status(404).json({ error: "Book not found" });
-    }
-    res.status(200).send(book);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-//Ex7
-app.get("/books/publishedYear/:year", async (req, res) => {
-  try {
-    const book = await Book.find({ publishedYear: req.params.year });
-
-    if (!book) {
-      return res.status(404).json({ error: "Book not found" });
-    }
-    res.status(200).send(book);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-//Ex8
-app.post("/books/:bookId", async (req, res) => {
-  try {
-    const rating = req.body;
-    const bookId = req.params.bookId;
-    let book = await Book.findById(bookId);
-    if (!book) {
-      return res.status(404).json({ error: "Book does not exist" });
-    }
-
-    if (!rating) {
-      return res.status(404).json({ error: "Rating is required" });
-    }
-
-    book = await Book.findByIdAndUpdate(bookId, rating, { new: true });
-    return res.status(200).json({
-      message: "Rating updated successfully",
-      book,
-    });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-//Ex9
-app.post("/books/update/:title", async (req, res) => {
-  try {
-    const title = req.params.title;
-    const { publishedYear, rating } = req.body;
-    let book = await Book.findOne({ title });
-
-    if (!book) {
-      return res.status(404).json({ error: "Book does not exist" });
-    }
-
-    if (!rating) {
-      return res.status(404).json({ error: "Rating is required" });
-    }
-
-    book = await Book.findOneAndUpdate({ title }, { rating }, { new: true });
-    return res.status(200).json({
-      message: "Rating updated successfully",
-      book,
-    });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-//Ex10
-app.delete("/books/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const book = await Book.findById(id);
-    if (!book) {
-      return res.status(404).json({ error: "Book does not exist" });
-    }
-    await Book.findByIdAndDelete(id);
-    return res.status(200).json({
-      message: "Book deleted successfully",
-      book,
-    });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+};
+getPosts();
---
+++ models/post.models.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+  },
+});
+
+const Post = mongoose.model("Post", postSchema);
+module.exports = Post;
